83441b488b7d857f1c62aa95a86483a94efd7b5c
David Parry <david.parry@suranyami.com>
2012-02-29 09:27:53 +1100

    sass-rails is now added by Rails by default, so don't need to add it

--- lib/templates/haml.rb
+++ lib/templates/haml.rb
@@ -1,7 +1,6 @@
 gem 'haml-rails', :group => [:asset]
-gem 'sass-rails'
 
 run 'bundle'
 
 git :add => 'Gemfile Gemfile.lock'
-git :commit => '-m "Adding HAML, SASS"'
+git :commit => '-m "Adding HAML"'
