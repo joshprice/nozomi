b6d9cde9c1ea554b72e830f1fb4bd68a32c1135a
Josh Price <josh@canoniq.com>
2012-09-05 13:45:22 +1000

    [Nozomi] Initial commit

---
+++ bant5/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bant5::Application.config.secret_token = '29ade1ed1b05d80d30442b11b41582b378ee2559b815e940345750280132fe93ccf4ddaebb705ea722b0810d32d53b08bc4fdcaa62a9f5b0720306910957e1c6'
